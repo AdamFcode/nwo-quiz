b601aca02daac4c7bd1d84c2567bdfbb607c92b2
AdamFcode <adamfeehely@gmail.com>
2024-04-06 16:45:46 +0000

    add js and style to target next button in quiz file and hide on load

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const nextButton = document.getElementById('next-btn');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    nextButton.classList.add("hide"); // Initially hide the 'Next' button
+  });
+
+  
