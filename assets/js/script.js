fab622a72878bba3d25e4f832fc76acbb1e32003
AdamFcode <adamfeehely@gmail.com>
2024-04-08 10:10:51 +0000

    declare initial variables in js file and function to hide next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-  
+ //*Declared variables for Dom Elements
+ const qContainer = document.getElementById('container');
+ const wrestlerImage = document.getElementById('headshot');
+ const wrestlerName = document.getElementById('wrestler-name')
+ const currentQuestion = document.getElementById('question-display');
+ const answerButtons = document.getElementById('answer-btns');
+const nextButton = document.getElementById('next-btn');
+
+let shuffledQuestions, currentQuestionIndex;
+
+ //*Function to hide next button when not needed
+  document.addEventListener('DOMContentLoaded', () => {
+    nextButton.classList.add("hide"); // Initially hide the 'Next' button
+  });
+
+ 
