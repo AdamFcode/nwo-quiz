1cce348f0581c217575a380a8357ef77f102d965
AdamFcode <adamfeehely@gmail.com>
2024-04-08 09:48:39 +0000

    remove test function to clear js sheet for final functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,1 @@
-const nextButton = document.getElementById('next-btn');
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    nextButton.classList.add("hide"); // Initially hide the 'Next' button
-  });
-
   
